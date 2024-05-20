86a1548c597c3bf6afc4176bd8d8c3164726369a
Rabia arif <160257505+zonikhan@users.noreply.github.com>
2024-05-20 14:48:36 +0500

    oop-project

---
+++ main.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+// Typescript Oop project 
+import inquirer from "inquirer";
+import chalk from "chalk";
+class person {
+    _personality;
+    constructor() {
+        this._personality = 'Mystery';
+    }
+    // userinput 
+    userInput(input) {
+        //if / else statement 
+        if (input.includes("talk to other about your self openly")) {
+            this._personality = "happly";
+        }
+        else if (input.includes("keep your word to yourself")) {
+            this._personality = "talk rude";
+        }
+    }
+    // return function 
+    get personality() {
+        return this._personality;
+    }
+}
+//create Objects 
+class Main {
+    async main() {
+        //await call out 
+        const result = await inquirer.prompt([
+            {
+                type: "list",
+                name: "choices",
+                message: chalk.yellowBright("tell your behaviour ?"),
+                choices: [
+                    "1: talk to others about my self.",
+                    "2: keep quite and dont ask questions."
+                ]
+            }
+        ]);
+        //create a persno class 
+        let myPerson = new person();
+        myPerson.userInput(result.choices);
+        //output response 
+        console.log((chalk.redBright)(`You are ${myPerson.personality}`));
+    }
+}
+const myObject = new Main();
+myObject.main();
+//completed Oop project
